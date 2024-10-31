357293ac63fbeaed8a5c48f9c984a9e8f5ffe8a0
Marco Scheidhuber <j23d@jusid.de>
2024-10-31 09:25:15 +0100

    Add bootstrap-react example.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Navbar, Nav, Container, Image } from 'react-bootstrap';
 
 import Home from './components/Home';
 import Contact from './components/Contact';
@@ -8,23 +10,23 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-    Application start
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Startseite</Link>
-          </li>
-          <li>
-            <Link to="/contact">Kontakt</Link>
-          </li>
-        </ul>
-      </nav>
+    <Navbar bg="light" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/"><Image src={logo} width="50" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
+    <Container className="mt-4">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    Application end
+    </Container>
 
     </div>
   );
