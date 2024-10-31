0efb7fef829e3c83ae2db56f400d62824f3c455c
Marco Scheidhuber <j23d@jusid.de>
2024-10-31 09:45:16 +0100

    Move navigation into own component.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-import { Routes, Route, Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Image } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 import Home from './components/Home';
 import Contact from './components/Contact';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-    <Navbar bg="light" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/"><Image src={logo} width="50" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
 
-    <Container className="mt-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Container>
+      <Navigation />
+
+      <Container className="mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Container>
 
     </div>
   );
